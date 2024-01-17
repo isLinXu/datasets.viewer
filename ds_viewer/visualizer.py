c758e96235408f9fa97df15933a459dd87374171
isLinXu <2267379130@qq.com>
2024-01-17 13:31:10 +0800

    [ds_viewer] update visualizer.py

--- ds_viewer/visualizer.py
+++ ds_viewer/visualizer.py
@@ -1,9 +1,9 @@
-
 import os
 import streamlit as st
 import cv2
 from PIL import Image
 import random
+
 
 class DatasetViewer:
     def __init__(self):
@@ -23,7 +23,8 @@ class DatasetViewer:
             if class_names:
                 self.class_names = [name.strip() for name in class_names.split(",")]
             num_classes = len(self.class_names) if self.class_names else 100
-            self.colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(num_classes)]
+            self.colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in
+                           range(num_classes)]
 
     def visual(self):
         if self.image_folder_path:
@@ -33,7 +34,8 @@ class DatasetViewer:
             images = [file for file in image_files if any(file.endswith(fmt) for fmt in supported_formats)]
 
             # 选择图像文件
-            image_index = st.sidebar.number_input("选择图像文件索引:", min_value=0, max_value=len(images) - 1, step=1, value=0)
+            image_index = st.sidebar.number_input("选择图像文件索引:", min_value=0, max_value=len(images) - 1, step=1,
+                                                  value=0)
             image_file = st.sidebar.selectbox("选择图像文件:", images, index=image_index)
 
             if image_file:
@@ -52,9 +54,6 @@ class DatasetViewer:
                     if annotation_file in annotations:
                         with open(os.path.join(self.label_folder_path, annotation_file), "r") as file:
                             content = file.read()
-                            st.write("标注内容:")
-                            st.write(content)
-
                             # 在图像上绘制边界框
                             image_cv = cv2.imread(image_path)
                             height, width, _ = image_cv.shape
@@ -71,15 +70,19 @@ class DatasetViewer:
                                         class_name = self.class_names[int(class_id)]
                                     else:
                                         class_name = str(int(class_id))
-                                    cv2.putText(image_cv, class_name, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,
-                                                2)
+                                    cv2.putText(image_cv, class_name, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX,0.5, color,2)
                             # 将图像从BGR颜色空间转换为RGB颜色空间
                             image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
-                        col1, col2 = st.columns(2)
-                        col1.image(image, caption="src", use_column_width=True)
-                        col2.image(image_cv, caption="dst", use_column_width=True)
+
+                            col1, col2 = st.columns(2)
+                            col1.image(image, caption="src", use_column_width=True)
+                            col2.image(image_cv, caption="dst", use_column_width=True)
+
+                            st.write("标注内容:")
+                            st.write(content)
                     else:
                         st.write("未找到对应的标注文件")
+
 
 def main():
     st.title("Dataset.Viewer©️(Hertz)")
