48c80dd3f953d72585cd2399c243deec4afad4d4
isLinXu <2267379130@qq.com>
2024-02-23 14:11:39 +0800

    [ds_viewer][utils] update tools.py

--- ds_viewer/utils/tools.py
+++ ds_viewer/utils/tools.py
@@ -11,6 +11,7 @@ def get_files(folder_path, supported_formats):
     files = os.listdir(folder_path)
     return [file for file in files if any(file.endswith(fmt) for fmt in supported_formats)]
 
+
 def load_images(image_folder_path, image_index, image_tags):
     if not image_folder_path:
         st.warning("请输入图像文件夹路径。")
