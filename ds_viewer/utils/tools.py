b507be016d9c42d713da2feb7fe6600de8c3cd37
isLinXu <2267379130@qq.com>
2024-02-25 23:02:21 +0800

    [ds_viewer][utils] update tools.py

--- ds_viewer/utils/tools.py
+++ ds_viewer/utils/tools.py
@@ -13,22 +13,17 @@ def get_files(folder_path, supported_formats):
 
 
 def load_images(image_folder_path, image_index, image_tags):
-    if not image_folder_path:
+    if os.path.exists(image_folder_path):
+        images = get_files(image_folder_path, [".jpg", ".png", ".jpeg", ".bmp", ".tiff"])
+        if images:
+            total_images = len(images)
+            progress_bar = st.progress(0)
+            for i, image_file in enumerate(images):
+                progress = (i + 1) / total_images
+                progress_bar.progress(progress)
+                if i == image_index:
+                    return image_file
+        else:
+            st.warning("图像文件夹中没有找到支持的图像格式。请检查路径和图像格式。")
+    else:
         st.warning("请输入图像文件夹路径。")
-        return None
-
-    images = get_files(image_folder_path, [".jpg", ".png", ".jpeg", ".bmp", ".tiff"])
-    if not images:
-        st.warning("图像文件夹中没有找到支持的图像格式。请检查路径和图像格式。")
-        return None
-
-    # 过滤掉已标记为 "成功" 的图像
-    filtered_images = [img for img in images if image_tags.get(img) != "成功"]
-
-    # 如果所有图像都被标记为成功，则显示一条警告信息并返回 None
-    if not filtered_images:
-        st.warning("所有图像均已标记为成功。")
-        return None
-
-    image_file = st.sidebar.selectbox("选择图像文件:", filtered_images, index=image_index)
-    return image_file
