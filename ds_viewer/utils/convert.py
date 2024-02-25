6b3a68acfd12dc99372be4e7e903f957fd51c6f8
isLinXu <2267379130@qq.com>
2024-02-25 23:03:57 +0800

    [ds_viewer][utils] add convert.py

---
+++ ds_viewer/utils/convert.py
@@ -0,0 +1,6 @@
+
+def convert_and_export_labels(state, st):
+    src_format = state.src_format
+    dst_format = state.dst_format
+    # TODO: 实现不同标签格式之间的转换，并将结果保存到指定的文件夹
+    st.warning("标签格式转换功能尚未实现。")
